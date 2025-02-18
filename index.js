0d45d4b47ba30a1bb55ece1c667a8da90f25a03a
Bhawana783 <kbhawana1411@gmail.com>
2025-02-18 21:38:57 +0530

    Completed the assignment

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
+require("dotenv").config();
 const express = require('express');
+const mongoose = require("mongoose");
 const { resolve } = require('path');
 
 const app = express();
-const port = 3010;
+const PORT = 3010;
+const MONGO_URI = process.env.MONGO_URI;
 
+app.use(express.json());
 app.use(express.static('static'));
 
+// MongoDB connection setup (without deprecated options)
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("Connected to database"))
+  .catch((error) => console.error("Error connecting to database:", error));
+
+// Serve static file from 'pages'
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
